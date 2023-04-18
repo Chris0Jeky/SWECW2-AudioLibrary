49154e384782202e95b30532d4ae9fe655f1d498
Chris <jeky.tck@gmail.com>
2023-04-18 05:53:24 +0100

    Hash table and File loader implementation

--- src/file_loader.cpp
+++ src/file_loader.cpp
@@ -5,5 +5,23 @@
 #include <iostream>
 
 void loadTracksFromFile(const std::string& fileName, HashTable& titleHashTable, HashTable& artistHashTable) {
-    // ... (existing implementation)
+    std::ifstream inputFile(fileName);
+    if (inputFile.is_open()) {
+        std::string line;
+        while (std::getline(inputFile, line)) {
+            std::istringstream lineStream(line);
+            std::string title, artist;
+            int duration;
+            std::getline(lineStream, title, '\t');
+            std::getline(lineStream, artist, '\t');
+            lineStream >> duration;
+
+            Track track(title, artist, duration);
+            titleHashTable.insertTrack(track, true);
+            artistHashTable.insertTrack(track, false);
+        }
+        inputFile.close();
+    } else {
+        std::cerr << "Unable to open file: " << fileName << std::endl;
+    }
 }
--- src/hash_table.cpp
+++ src/hash_table.cpp
@@ -6,9 +6,24 @@ HashTable::HashTable(int size) : size(size) {
 }
 
 void HashTable::insertTrack(const Track& track, bool useTitleAsKey) {
-    // ... (existing implementation)
+    unsigned int index = useTitleAsKey ? hashFunction(track.title) : hashFunction(track.artist);
+    index %= size;
+
+    for (const Track& existingTrack : buckets[index]) {
+        if ((useTitleAsKey && existingTrack.title == track.title) ||
+            (!useTitleAsKey && existingTrack.artist == track.artist && existingTrack.title == track.title)) {
+            return; // track already exists in the library
+        }
+    }
+    buckets[index].push_back(track);
 }
 
 unsigned int HashTable::hashFunction(const std::string& key) {
-    // ... (existing implementation)
+    unsigned int hashFunction(const std::string& key) {
+        unsigned int hash = 0;
+        for (char c : key) {
+            hash = 31 * hash + c;
+        }
+        return hash;
+    }
 }
