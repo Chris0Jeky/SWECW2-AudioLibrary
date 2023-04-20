bf31be902bac84e41adfb41c3d32e881f4dcbca6
Chris <jeky.tck@gmail.com>
2023-04-20 01:53:55 +0100

    Added 0, 1 and 2 arguments options.

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,32 @@
 #include <iostream>
 #include <ostream>
 
-int main() {
+int main(int argc, char* argv[]) {
+    std::string fileName;
+
+    if (argc == 1) {
+        std::cout << "You can insert the name of the file you wish to work with or you can specify"
+                     " the path to that file and the name of the file." << std::endl;
+        fileName = "tracks.txt";
+    } else if (argc == 2) {
+        fileName = argv[1];
+    } else if (argc == 3) {
+        std::string filePath = argv[1];
+        std::string file = argv[2];
+
+        if (filePath.back() != '/' && filePath.back() != '\\') {
+            filePath += "/";
+        }
+        fileName = filePath + file;
+    } else {
+        std::cerr << "Usage: " << argv[0] << " [<path> <filename>]" << std::endl;
+        return 1;
+    }
+
     HashTable titleHashTable;
     HashTable artistHashTable;
 
-    loadTracksFromFile("tracks.txt", titleHashTable, artistHashTable);
+    loadTracksFromFile(fileName, titleHashTable, artistHashTable);
 
     // Example: Search for a track by title
     std::string titleToSearch = "Some Track Title";
